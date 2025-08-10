5b9e932b72080b18a88d51e006e4c06d34a4f803
Andrei Iatan <andrei.iatan00@gmail.com>
2025-08-10 22:49:24 +0300

    initial commit: basic ws socket

---
+++ server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { generateRoomId } from './utils';
+import { initWsServer } from './socket';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_, res) => {
+	res.send('Hello World!')
+});
+
+app.get('/createRoom', async (_, res) => {
+	const roomId = generateRoomId();
+
+	try {
+		await initWsServer(roomId);
+		res.json({ roomId, message: `Room created with id: ${roomId}` });
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Unable to create room.' })
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+});
+
---
+++ socket.ts
@@ -0,0 +1,26 @@
+import { WebSocketServer } from "ws";
+
+export function initWsServer(roomId: string): Promise<WebSocketServer> {
+	return new Promise((resolve, reject) => {
+		const wss = new WebSocketServer({ host: 'localhost', path: `/${roomId}`, port: 8080 });
+
+		wss.once('error', (err) => {
+			reject(new Error(`Failed to start WebSocketServer for room "${roomId}": ${err.message}`));
+		});
+
+		wss.once('listening', () => {
+			resolve(wss);
+		});
+
+		wss.on('connection', (ws) => {
+			ws.on('message', (data) => {
+				console.log('received: %s', data);
+				ws.send(data);
+			});
+
+			setInterval(() => {
+				ws.send('something');
+			}, 1000);
+		});
+	});
+}
